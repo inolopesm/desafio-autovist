fa67efe1863f2c352ea8c733461b7f9efcf9501a
Matheus Lopes <inolopesm@gmail.com>
2023-10-19 12:03:23 -0300

    refactor: initialize mongo + change dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,64 @@
 import * as express from "express";
-import { ExpressControllerAdapter } from "./infra/express-controller-adapter";
+
 import {
   CreateClientController,
   CreateClientRequest,
 } from "./app/controllers/create-client-controller";
+
+import { AddressInMemoryRepository } from "./infra/address-in-memory-repository";
 import { AddressViaCEPRepository } from "./infra/address-via-cep-repository";
-import { ClientInMemoryRepository } from "./infra/client-in-memory-repository";
+import { ClientMongoRepository } from "./infra/client-mongo-repository";
+import { ExpressControllerAdapter } from "./infra/express-controller-adapter";
+import { FindOneAddressByCEPRepositoryComposite } from "./infra/find-one-address-by-cep-repository-composite";
+import { MongoHelper } from "./infra/mongo-helper";
 import { YupValidationAdapter } from "./infra/yup-validation-adapter";
 
-const addressViaCEPRepository = new AddressViaCEPRepository();
-const clientInMemoryRepository = new ClientInMemoryRepository();
+MongoHelper.getInstance()
+  .connect(process.env.MONGO_URL as string)
+  .then(() => {
+    const addressInMemoryRepository = new AddressInMemoryRepository();
+    const addressViaCEPRepository = new AddressViaCEPRepository();
 
-const app = express();
+    const findOneAddressByCEPRepositoryComposite =
+      new FindOneAddressByCEPRepositoryComposite([
+        addressInMemoryRepository,
+        addressViaCEPRepository,
+      ]);
 
-app.use(express.json());
+    const clientMongoRepository = new ClientMongoRepository();
 
-app.post(
-  "/api/v1/clients",
-  ExpressControllerAdapter.adapt(
-    new CreateClientController({
-      validation: new YupValidationAdapter<CreateClientRequest>((yup) =>
-        yup.object({
-          params: yup.object(),
-          query: yup.object(),
-          body: yup.object({
-            name: yup.string().defined().min(1).max(255),
-            email: yup.string().defined().min(1).max(255).email(),
-            phone: yup
-              .string()
-              .defined()
-              .matches(/^\d{11}$/),
-            cep: yup
-              .string()
-              .defined()
-              .matches(/^\d{8}$/),
-          }),
+    const app = express();
+
+    app.use(express.json());
+
+    app.post(
+      "/api/v1/clients",
+      ExpressControllerAdapter.adapt(
+        new CreateClientController({
+          validation: new YupValidationAdapter<CreateClientRequest>((yup) =>
+            yup.object({
+              params: yup.object(),
+              query: yup.object(),
+              body: yup.object({
+                name: yup.string().defined().min(1).max(255),
+                email: yup.string().defined().min(1).max(255).email(),
+                phone: yup
+                  .string()
+                  .defined()
+                  .matches(/^\d{11}$/),
+                cep: yup
+                  .string()
+                  .defined()
+                  .matches(/^\d{8}$/),
+              }),
+            })
+          ),
+          findOneAddressByCEPRepository: findOneAddressByCEPRepositoryComposite,
+          findOneClientByEmailRepository: clientMongoRepository,
+          createClientRepository: clientMongoRepository,
         })
-      ),
-      findOneAddressByCEPRepository: addressViaCEPRepository,
-      findOneClientByEmailRepository: clientInMemoryRepository,
-      createClientRepository: clientInMemoryRepository,
-    })
-  )
-);
+      )
+    );
 
-app.listen(3000, "0.0.0.0");
+    app.listen(3000, "0.0.0.0");
+  });
